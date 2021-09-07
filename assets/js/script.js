2b98098be10ad1dd8bf837a01849274055045d06
Rainacorn <reyna.ortiz84@gmail.com>
2021-09-07 15:20:15 -0700

    added jquery drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -132,6 +132,73 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+  // array to store the task data in
+var tempArr = [];
+
+// loop over current set of children in sortable list
+$(this)
+  .children()
+  .each(function() {
+    // save values in temp array
+    tempArr.push({
+    text: $(this)
+      .find("p")
+      .text()
+      .trim(),
+
+    date: $(this)
+      .find("span")
+      .text()
+      .trim()
+    });
+  });
+
+  //trim down lists ID to match object property
+  
+  var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
+
+  //update array on task object and save
+  tasks[arrName] = tempArr;
+  saveTasks();
+  console.log(tempArr)
+},
+})
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
@@ -177,5 +244,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
